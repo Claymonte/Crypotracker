7285ef9a7a0fe9daa266270199d83afb752adfe2
Kasper Wickholm <kaswic@hotmail.com>
2019-12-13 12:31:10 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Constants } from 'expo';
+import CoinView from './screens/CoinView';
+import TopBar from './components/TopBar';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      refreshDate: '-',
+    }
+  }
+
+  _setRefreshDate = (date) => { // Called from CoinView's prop
+    console.log('Updated: '+ date);
+    this.setState({
+      refreshDate: date,
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TopBar title="Top 10 crypto's" refreshDate={this.state.refreshDate} />
+        <CoinView
+          refreshDate={this._setRefreshDate}
+          style={styles.coinView}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  coinView: {
+    width: '100%',
+    flex: 1,
+    flexDirection: 'column', 
+    backgroundColor: 'white',
+    //alignItems: 'center',
+    //justifyContent: 'flex-start' // center, space-around
+  }
+});
